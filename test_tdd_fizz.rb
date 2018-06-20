bf80bd4fd1e1e3c20e4e6093c989ab2932098c87
Jordan Endicott <jordan.endicott823@gmail.com>
2018-06-20 15:56:47 -0400

    fail multiples of 15 are mined minds

--- test_tdd_fizz.rb
+++ test_tdd_fizz.rb
@@ -40,4 +40,9 @@ class TestTddFizzBuzz < Minitest::Test
     	assert_equal("minds", fizzbuzz()[9])
     	assert_equal("minds", fizzbuzz()[39])
     end
+
+    def test_assert_multiples_of_15_returns_mined_minds
+    	assert_equal("mined minds", fizzbuzz()[14])
+    	assert_equal("mined minds", fizzbuzz()[44])
+    end
 end
